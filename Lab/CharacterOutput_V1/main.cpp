153ee46ef0c2a3ffb0716b77ea82d973ded124ee
rcc <rcc>
2017-01-24 12:09:48 -0800

    Signed-off-by: rcc <rcc>

--- Lab/CharacterOutput_V1/main.cpp
+++ Lab/CharacterOutput_V1/main.cpp
@@ -2,7 +2,7 @@
   File:   main.cpp
   Author: Dr. Mark E. Lehr
   Created on January 23, 2017, 11:30 AM
-  Purpose:  Investigate the character/cout ascii output with functions
+  Purpose:  Investigate the character/cout ascii output
  */
 
 //System Libraries
@@ -16,28 +16,25 @@ using namespace std;
 //as well as conversions from system of units to another
 
 //Function Prototypes
-void repeat(char,int);
 
 //Executable code begins here!!!
 int main(int argc, char** argv) {
+    //Process by mapping inputs to outputs
+    for(char c=0;c<127;c++){
+        cout<<c;
+        if(c%16==15)cout<<endl;
+    }
     //Output a triangle 
     //Declare the base;
-    int base=15,seprshn=15;
+    int base=75;
     cout<<endl<<endl;
     for(int nLines=1;nLines<=base;nLines++){
-        repeat('*',nLines);
-        repeat(' ',base-nLines);
-        repeat(' ',seprshn);
-        repeat('*',base-nLines+1);
+        for(int nCols=base-nLines+1;nCols>=1;nCols--){
+            cout<<"*";
+        }
         cout<<endl;
     }
 
     //Exit stage right!
     return 0;
-}
-
-void repeat(char c,int n){
-    for(int col=1;col<=n;col++){
-        cout<<c;
-    }
 }
