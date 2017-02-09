f6ef52a03ac853b104c1419b0f1f6d5b2590cd4f
Linh Vu <Linh Vu>
2017-02-08 21:31:14 -0800

    Signed-off-by: Linh Vu <Linh Vu>

--- Project/Project II/CSC5_Project2_Final_TicTacToe/main.cpp
+++ Project/Project II/CSC5_Project2_Final_TicTacToe/main.cpp
@@ -18,13 +18,13 @@ using namespace std;
 
 //User Libraries
 //Set colors
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_RED     "\x1b[31m"    //Color red
+#define ANSI_COLOR_GREEN   "\x1b[32m"    //Color green
+#define ANSI_COLOR_YELLOW  "\x1b[33m"    //Color yellow
+#define ANSI_COLOR_BLUE    "\x1b[34m"    //Color blue
+#define ANSI_COLOR_MAGENTA "\x1b[35m"    //Color magenta
+#define ANSI_COLOR_CYAN    "\x1b[36m"    //Color cyan
+#define ANSI_COLOR_RESET   "\x1b[0m"     //Reset back to black
 
 //Global Constants
 //Such as PI, Vc, -> Math/Science values
@@ -40,11 +40,13 @@ char gBoard[ROWSIZE][COLSIZE]= { {'A','B','C','D'},
 char turn;       //Player turns
 bool DRAW=false, //Game Won
      gOver ();   //Game over
+
 void dBoard();   //Display Board
 void pTurn ();   //Player switching turns
-void filAray(int [],int);                     //Fill the array with random number
-void prntAry(int [],int,int);
-int  luckyn (int [],int,int);                 //Lucky number
+void bubble (int [],int);      //Bubble 
+void filAray(int [],int);      //Fill the array with random number
+void prntAry(int [],int,int);  //Print out the array
+int  luckyn (int [],int,int);  //Lucky number
 
 //Executable code begins here!!!
 int main(int argc, char** argv) {
@@ -69,72 +71,105 @@ int main(int argc, char** argv) {
             "first and last name"
             " as Player 1 and Player 2."<<endl;
     cout<<endl;
-    cout<<ANSI_COLOR_MAGENTA<<"Player 1: [X] - "<<ANSI_COLOR_RESET; //Player 1's name [X]
+    cout<<ANSI_COLOR_MAGENTA<<"PLAYER 1: [X] - "<<ANSI_COLOR_RESET; 
+    //Player 1's name [X]
     cin>>first1>>last1;
-    cout<<ANSI_COLOR_MAGENTA<<"Player 2: [O] - "<<ANSI_COLOR_RESET; //Player 2's name [O]]
+    cout<<ANSI_COLOR_MAGENTA<<"PLAYER 2: [O] - "<<ANSI_COLOR_RESET; 
+    //Player 2's name [O]]
     cin>>first2>>last2;
     cout<<endl;
     cout<<"HELLO, "<<first1<<" and "<<first2<<"."<<endl;
     cout<<"WELCOME TO THE GAME! HERE ARE THE RULES: "<<endl;
     cout<<endl;
-    cout<<"1) Two players take turns choosing a spot to place an 'X' or an 'O'."<<endl;
-    cout<<"2) The game is over when a player has four spots in a line, either a "
-            "whole row, a whole column or diagonally."<<endl;
+    cout<<"1) Two players take turns choosing a spot to place an 'X' or an "
+            "'O'."<<endl;
+    cout<<"2) The game is over when a player has four spots in a line, either "
+            "a whole row, a whole column or diagonally."<<endl;
     cout<<"Now you know the rules. Would you like the play the game?"<<endl;
     cout<<"       Y - YES      N - NO"<<endl;
     cout<<ANSI_COLOR_RED<<"YOUR CHOICE: "<<ANSI_COLOR_RESET ;
     cin>>yesno;
     
     //Begin the game (Y)
-    switch (yesno) {
-        case 'y': case 'Y': {
-            cout<<endl;
-        }
-        case 'N': case 'n':
-;    }   
-        
-    //Mapping out the game
-    turn='X'; //Player 1's turn
-    while (!gOver()) {
-           dBoard();
-            pTurn();
-            gOver();
-    }
-    if (turn=='O'&&!DRAW) {
-        dBoard(); //Board Display
-        cout<<endl<<endl<<"Player 1 [X] "<<first1<<" "<<last1<<" Wins!\n";
-        //Winner's Prize
-        int find;
-        if (turn=='O'&&!DRAW) {
-            //Input Value
-            filAray(array,utilize);  
-            //Output
-            cout<<endl;
-            cout<<"YOU ARE THE WINNER!"<<endl;
-            cout<<"Now let's try your luck to get a prize.\nType in a two digits "
-                    "number,\nand if your two digits number is found in a range "
-                    "from 10 to 99,\nyou will earn a prize!"<<endl;
-            cout<<"Type in your number: ";
-            cin>>find;
-            cout<<endl;
-            cout<<"CONGRATULATION! "<<find<<" was found at "<<luckyn
-                (array,utilize,find)<<"."<<endl;
-            //Output array
-            prntAry(array,utilize,10);
-        }
-    }
-    else if (turn=='X'&&!DRAW) {
-        dBoard(); //Board Display
-        cout<<endl<<endl<<"Player 2 [O] "<<first2<<" "<<last2<<" Wins!\n";
-    }
-    else {
-        dBoard(); //Board Display
-        cout<<endl<<endl<<"[X] and [O] ==> DRAW!\n";
-    }
-
+    bool x=false; 
+    do {
+        switch (yesno) {
+            case 'y': case 'Y': {
+                cout<<endl;
+            }   
+                //Mapping out the game
+                turn='X'; //Player 1's turn
+                while (!gOver()) {
+                       dBoard();
+                        pTurn();
+                        gOver();
+                }
+                if (turn=='O'&&!DRAW) {
+                    dBoard(); //Board Display
+                    cout<<endl<<endl<<"Player 1 [X] "<<first1<<" "<<last1<<
+                            " Wins!\n";
+                    //Winner's Prize
+                    int find;
+                    if (turn=='O'&&!DRAW) {
+                        //Input Value
+                        filAray(array,utilize);  
+                        //Output
+                        cout<<endl;
+                        cout<<"YOU ARE THE WINNER!"<<endl;
+                        cout<<"Now let's try your luck to get a prize.\nType "
+                                "in a two digits number,\nand if your two "
+                                "digits number is found in a range from 10 to "
+                                "99,\nyou will earn a prize!"<<endl;
+                        cout<<"Type in your number: ";
+                        cin>>find;
+                        cout<<endl;
+                        cout<<"CONGRATULATION! "<<find<<" was found at "<<luckyn
+                            (array,utilize,find)<<"."<<endl;
+                        //Output array
+                        prntAry(array,utilize,10);
+                    }
+                }
+                else if (turn=='X'&&!DRAW) {
+                    dBoard(); //Board Display
+                    cout<<endl<<endl<<"Player 2 [O] "<<first2<<" "<<last2<<" Wins!\n";
+                    //Winner's Prize
+                    int find;
+                    if (turn=='X'&&!DRAW) {
+                        //Input Value
+                        filAray(array,utilize);  
+                        //Output
+                        cout<<endl;
+                        cout<<"YOU ARE THE WINNER!"<<endl;
+                        cout<<"Now let's try your luck to get a prize.\nType "
+                                "in a two digits number,\nand if your two "
+                                "digits number is found in a range from 10 to "
+                                "99,\nyou will earn a prize!"<<endl;
+                        cout<<"Type in your number: ";
+                        cin>>find;
+                        cout<<endl;
+                        cout<<"CONGRATULATION! "<<find<<" was found at "<<luckyn
+                            (array,utilize,find)<<"."<<endl;
+                        //Output array
+                        prntAry(array,utilize,10);
+                    }
+                }
+                else {
+                    dBoard(); //Board Display
+                    cout<<endl<<endl<<"[X] and [O] ==> DRAW!\n";
+                    }
+                break;
+                case 'N':
+                case 'n': {
+                    x=false;
+                    cout<<endl;
+                    cout<<"\tSEE YOU AGAIN."<<endl; 
+                    }
+                }
+    }while (turn=='O'&&!DRAW||turn=='X'&&!DRAW);
+     while (x);
+    
     return 0;
 }
-
 
 //******************************************************************************
 //*****************************DISPLAY THE BOARD********************************
@@ -146,16 +181,20 @@ void dBoard () {
     cout<<ANSI_COLOR_RESET<<"Player 1 (X):  -  Player 2 (O):  "<<endl;
     cout<<"  ---------------------------------"<<endl;
     cout<<"\t     |     |     |     "<<endl;
-    cout<<"\t  "<<gBoard[0][0]<<"  |  "<<gBoard[0][1]<<"  |  "<<gBoard[0][2]<<"  |  "<<gBoard[0][3]<<endl;
+    cout<<"\t  "<<gBoard[0][0]<<"  |  "<<gBoard[0][1]<<"  |  "
+                <<gBoard[0][2]<<"  |  "<<gBoard[0][3]<<endl;
     cout<<"\t_____|_____|_____|_____"<<endl;
     cout<<"\t     |     |     |     "<<endl;
-    cout<<"\t  "<<gBoard[1][0]<<"  |  "<<gBoard[1][1]<<"  |  "<<gBoard[1][2]<<"  |  "<<gBoard[1][3]<<endl;
+    cout<<"\t  "<<gBoard[1][0]<<"  |  "<<gBoard[1][1]<<"  |  "
+                <<gBoard[1][2]<<"  |  "<<gBoard[1][3]<<endl;
     cout<<"\t_____|_____|_____|_____"<<endl;
     cout<<"\t     |     |     |     "<<endl;
-    cout<<"\t  "<<gBoard[2][0]<<"  |  "<<gBoard[2][1]<<"  |  "<<gBoard[2][2]<<"  |  "<<gBoard[2][3]<<endl;
+    cout<<"\t  "<<gBoard[2][0]<<"  |  "<<gBoard[2][1]<<"  |  "
+                <<gBoard[2][2]<<"  |  "<<gBoard[2][3]<<endl;
     cout<<"\t_____|_____|_____|_____"<<endl;
     cout<<"\t     |     |     |     "<<endl;
-    cout<<"\t  "<<gBoard[3][0]<<"  |  "<<gBoard[3][1]<<"  |  "<<gBoard[3][2]<<"  |  "<<gBoard[3][3]<<endl;
+    cout<<"\t  "<<gBoard[3][0]<<"  |  "<<gBoard[3][1]<<"  |  "
+                <<gBoard[3][2]<<"  |  "<<gBoard[3][3]<<endl;
     cout<<"\t     |     |     |     "<<endl<<endl;
 }
 
@@ -167,10 +206,10 @@ void pTurn() {
    
     //Players Turns 
     if (turn=='X') { //Player 1
-        cout<<ANSI_COLOR_MAGENTA<<"Player 1's Turn [X]: "<<ANSI_COLOR_BLUE;
+        cout<<ANSI_COLOR_MAGENTA<<"Player 1's Turn [X]: "<<ANSI_COLOR_RESET;
     }
     else { //Player 2
-        cout<<ANSI_COLOR_MAGENTA<<"Player 2's Turn [O]: "<<ANSI_COLOR_YELLOW;
+        cout<<ANSI_COLOR_MAGENTA<<"Player 2's Turn [O]: "<<ANSI_COLOR_RESET;
     }
     cin>>choice;
     
@@ -266,11 +305,24 @@ void prntAry(int a[],int n,int perLine){
 
 int luckyn(int a[],int n,int val) {
     for (int i=0;i<n;i++) {
-        if (a[i]==val)
+        if (a[i]==val) 
             return i;
-    else {
-        cout<<"Too bad, you're out of luck..."<<endl;
-        }
     }
     return 0;
+}
+
+void bubble(int array[],int number){
+    bool swap;
+    int temp;
+    do{
+        swap=false;
+        for(int i=0;i<number-1;i++){
+            if(array[i]>array[i+1]){
+                temp=array[i];
+                array[i]=array[i+1];
+                array[i+1]=temp;
+                swap=true;
+            }
+        }
+    }while(swap);
 }
